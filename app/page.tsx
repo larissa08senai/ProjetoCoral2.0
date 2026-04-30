9df479afa7468a7fbd7c894147a14dd9f59b0179
unknown <larissa.siqueira62608@gmail.com>
2026-04-30 10:36:22 -0300

    Atualização para Next

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,36 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
+    <div className="principal">
+      <main className="inicial">
+        <div className="header">
+          <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/LogoCoral.png"
+          alt="Coral Food logo"
           width={100}
-          height={20}
+          height={80}
           priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+          />
+          <nav>
+            <Link href="/">Inicial</Link>
+            <Link href="/menu">Menu</Link>
+            <Link href="/sobre">Sobre</Link>
+            <Link href="/contato">Contato</Link>
+          </nav>
+        </div>
+        <hr />
+        <div className="conteudo">
+          <h1 className="Slogan">
+            <div className="linha"><div className="F">Fresco.</div><div className="S">Saudável.</div></div>
+            <div className="I">Irresistível.</div>
+            <br />
+            Descubra o sabor do mar com a Coral Food!
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <p className="legenda">
+            Descubra os sabores do ocenao no coração da cidade. Marisco fresco, pratos autênticos e uma experiência única
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
